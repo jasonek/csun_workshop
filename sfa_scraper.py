6e34afa58b3f712dd7137e2f60d8a48c71f9800f
Jason Kucharski <jasonek@pm.me>
2019-03-17 12:07:36 -0700

    We dont need to print everything to screen for our program, so removing those lines

--- sfa_scraper.py
+++ sfa_scraper.py
@@ -2,11 +2,7 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
 summary_url = 'https://finance.yahoo.com/quote/TSLA?p=TSLA'
-print(summary_url)
 
 response = urlopen(summary_url)
-print(response)
 
-for key, value in response.headers.items():
-    print(key + ": " + value)
-# Notice how similar it is to:  `curl -I -XGET https://finance.yahoo.com/quote/TSLA?p=TSLA`
+tsla_html = BeautifulSoup(response, "html.parser")
