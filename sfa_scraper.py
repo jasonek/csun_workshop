7a0a4321f360bf50bb706e1ac75c54c80a2dee5d
Jason Kucharski <jasonek@pm.me>
2019-03-17 14:54:31 -0700

    We can pull out the exact info we want by finding the the CSS attribute that contains the text we want. In this case the id=quote-summary is the parent that contains all of the financial info. Within that, it is the data-test=BID-value that has the bid info. We call getText() on that attribute to get the text that we are seeing in the browser. The price x volume

--- sfa_scraper.py
+++ sfa_scraper.py
@@ -6,3 +6,9 @@ summary_url = 'https://finance.yahoo.com/quote/TSLA?p=TSLA'
 response = urlopen(summary_url)
 
 tsla_html = BeautifulSoup(response, "html.parser")
+
+quote = tsla_html.find("div", {"id": "quote-summary"})
+# OR you can do this more directly
+qt = tsla_html.find(id = "quote-summary")
+price, volume = bid.split('x')
+print("The bid price is {} and current volume is {}".format(price, volume))
