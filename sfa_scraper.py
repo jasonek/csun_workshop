5968578e0e129f2f02b86fa141dba7e29c39e6f7
Jason Kucharski <jasonek@pm.me>
2019-03-16 21:43:59 -0700

    Print the status code of the response and show the URL again

--- sfa_scraper.py
+++ sfa_scraper.py
@@ -1,7 +1,11 @@
 from urllib.request import urlopen
+from bs4 import BeautifulSoup
 
 summary_url = 'https://finance.yahoo.com/quote/TSLA?p=TSLA'
 print(summary_url)
 
-summary_page = urlopen(summary_url)
-print(summary_page)
+response = urlopen(summary_url)
+print(response)
+
+print(response.code)
+print(response.url)
