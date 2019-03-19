4aaa8381f750988e59c7a4620ebba77ecf47d7b4
Jason Kucharski <jasonek@pm.me>
2019-03-18 22:59:59 -0700

    We can add more tickers to the company list. And now we can iterate through that list, using the ticker string to dynamically create URLs. In addition, once we get the data for a given ticker, we can dynamically create keys inside the stock_bids dictionary and set the price and volume in it

--- sfa_scraper.py
+++ sfa_scraper.py
@@ -1,21 +1,21 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
-companies = ['TSLA']
-
-summary_url = 'https://finance.yahoo.com/quote/' + companies[0] + '?p=' + companies[0]
-
-response = urlopen(summary_url)
-
-tsla_html = BeautifulSoup(response, "html.parser")
-
-quote = tsla_html.find("div", {"id": "quote-summary"})
-# OR you can do this more directly
-qt = tsla_html.find(id = "quote-summary")
-bid = qt.find(attrs={"data-test":"BID-value"}).getText()
-price, volume = bid.split('x')
-print("The bid price is {} and current volume is {}".format(price, volume))
-
+companies = ['TSLA', 'XOM', 'ORCL']
 stock_bids = {}
 
-stock_bids[companies[0]] = {"bid" : price, "volume": volume}
+for company in companies:
+    summary_url = 'https://finance.yahoo.com/quote/' + company + '?p=' + company
+
+    response = urlopen(summary_url)
+
+    tsla_html = BeautifulSoup(response, "html.parser")
+
+    qt = tsla_html.find(id = "quote-summary")
+    bid = qt.find(attrs={"data-test":"BID-value"}).getText()
+    price, volume = bid.split('x')
+    print("The bid price is {} and current volume is {}".format(price, volume))
+
+    stock_bids[company] = {"bid" : price, "volume": volume}
+
+print(stock_bids)
