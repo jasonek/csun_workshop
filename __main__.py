f083b8388ed241716e4b70b90207aea468e447ba
Jason Kucharski <jasonek@pm.me>
2019-03-19 15:28:37 -0700

    In order for the __main__ function to access the logic we have written, we need to wrap sfa_scraper in a function. Notice that we chose to have our new function take a parameter of a list of tickers. And we moved the creation of that list to the main file. Another difference is fetch_bids is not printing anything, but instead returning a value. Whichever program that calls fetch_bids() and use the results however it wants. It can print them, it can make them inputs into another function, it can write them to a csv file, etc;

--- __main__.py
+++ __main__.py
@@ -1,7 +1,14 @@
 import sys
+from sfa_scraper import fetch_bids
 
 def main():
-    print(1)
+    tickers = ['TSLA', 'XOM', 'ORCL']
+    bids = fetch_bids(tickers)
+    print(bids)
+
+    # it is cleaner code to break up operations into
+    # well-named, descriptive/contextual variables. but we could do:
+    # print(fetch_bids('TSLA', 'XOM', 'ORCL']))
 
 if __name__ == '__main__':
     sys.exit(main())
--- sfa_scraper.py
+++ sfa_scraper.py
@@ -1,23 +1,22 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
-companies = ['TSLA', 'XOM', 'ORCL']
-stock_bids = {}
+def fetch_bids(companies):
+    stock_bids = {}
 
-for company in companies:
-    summary_url = 'https://finance.yahoo.com/quote/' + company + '?p=' + company
+    for company in companies:
+        summary_url = 'https://finance.yahoo.com/quote/' + company + '?p=' + company
 
-    response = urlopen(summary_url)
+        response = urlopen(summary_url)
 
-    tsla_html = BeautifulSoup(response, "html.parser")
+        tsla_html = BeautifulSoup(response, "html.parser")
 
-    qt = tsla_html.find(id = "quote-summary")
-    bid = qt.find(attrs={"data-test":"BID-value"}).getText()
-    
-    price, volume = [float(item) for item in bid.split('x')]
+        qt = tsla_html.find(id = "quote-summary")
+        bid = qt.find(attrs={"data-test":"BID-value"}).getText()
 
-    print("The bid price is {} and current volume is {}".format(price, volume))
+        price, volume = [float(item) for item in bid.split('x')]
 
-    stock_bids[company] = {"bid" : price, "volume": volume}
+        print("The bid price is {} and current volume is {}".format(price, volume))
 
-print(stock_bids)
+        stock_bids[company] = {"bid" : price, "volume": volume}
+        return stock_bids
