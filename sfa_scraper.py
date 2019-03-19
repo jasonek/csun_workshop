1ec7d506b3da7ae63e58d78733b1abe0b088add6
Jason Kucharski <jasonek@pm.me>
2019-03-18 22:51:06 -0700

    Replace strings of TSLA with accessing a list that contains TSLA at index 0

--- sfa_scraper.py
+++ sfa_scraper.py
@@ -1,7 +1,9 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
-summary_url = 'https://finance.yahoo.com/quote/TSLA?p=TSLA'
+companies = ['TSLA']
+
+summary_url = 'https://finance.yahoo.com/quote/' + companies[0] + '?p=' + companies[0]
 
 response = urlopen(summary_url)
 
@@ -10,9 +12,10 @@ tsla_html = BeautifulSoup(response, "html.parser")
 quote = tsla_html.find("div", {"id": "quote-summary"})
 # OR you can do this more directly
 qt = tsla_html.find(id = "quote-summary")
+bid = qt.find(attrs={"data-test":"BID-value"}).getText()
 price, volume = bid.split('x')
 print("The bid price is {} and current volume is {}".format(price, volume))
 
 stock_bids = {}
 
-stock_bids["TSLA"] = {"bid" : price, "volume": volume}
+stock_bids[companies[0]] = {"bid" : price, "volume": volume}
