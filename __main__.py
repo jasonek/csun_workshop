aec956202625df52aa2a4496eb77bded86bc2aee
Jason Kucharski <jasonek@pm.me>
2019-03-19 15:49:16 -0700

    On the previous note of breaking things up, we noticed that sfa_scraper is getting a bit large of a function and doing multipe things. Inside a for loopis often a good place to cut some logic and paste it into a new function. And that helper function should be self-contained and independent. We could directly call *it* from the main function

--- __main__.py
+++ __main__.py
@@ -1,14 +1,16 @@
 import sys
-from sfa_scraper import fetch_bids
+from sfa_scraper import get_stock_bids, fetch_bid
 
 def main():
     tickers = ['TSLA', 'XOM', 'ORCL']
-    bids = fetch_bids(tickers)
+    bids = get_stock_bids(tickers)
     print(bids)
 
     # it is cleaner code to break up operations into
     # well-named, descriptive/contextual variables. but we could do:
-    # print(fetch_bids('TSLA', 'XOM', 'ORCL']))
+    # print(get_stock_bids('TSLA', 'XOM', 'ORCL']))
+
+    print(fetch_bid('AAPL'))
 
 if __name__ == '__main__':
     sys.exit(main())
--- sfa_scraper.py
+++ sfa_scraper.py
@@ -1,12 +1,20 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
-def fetch_bids(companies):
+def get_stock_bids(companies):
     stock_bids = {}
 
     for company in companies:
-        summary_url = 'https://finance.yahoo.com/quote/' + company + '?p=' + company
+        price, volume = fetch_bid(company)
+        print("The bid price is {} and current volume is {}".format(price, volume))
 
+        stock_bids[company] = {"bid" : price, "volume": volume}
+
+    return stock_bids
+
+
+def fetch_bid(ticker):
+        summary_url = 'https://finance.yahoo.com/quote/' + ticker + '?p=' + ticker
         response = urlopen(summary_url)
 
         tsla_html = BeautifulSoup(response, "html.parser")
@@ -14,10 +22,4 @@ def fetch_bids(companies):
         qt = tsla_html.find(id = "quote-summary")
         bid = qt.find(attrs={"data-test":"BID-value"}).getText()
 
-        price, volume = [float(item) for item in bid.split('x')]
-
-        print("The bid price is {} and current volume is {}".format(price, volume))
-
-        stock_bids[company] = {"bid" : price, "volume": volume}
-
-    return stock_bids
+        return [float(item) for item in bid.split('x')]
