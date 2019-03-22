a2a77eb6f2f24a7c28f1d1c0a7e4059efe4dbb81
Jason Kucharski <jasonek@pm.me>
2019-03-22 12:45:50 -0700

    Add a function to get the Market Cap from Yahoo finance. It is very similar to the bid-grabbing function, with a few differences. In main, I replaced the calling of get_stock_bids() with get_market_caps()

--- scraper/__main__.py
+++ scraper/__main__.py
@@ -1,16 +1,15 @@
 import sys
-from sfa_scraper import get_stock_bids, fetch_bid
+from sfa_scraper import get_stock_bids, get_market_caps
 
 def main():
     tickers = ['TSLA', 'XOM', 'ORCL']
-    bids = get_stock_bids(tickers)
-    print(bids)
+    market_caps = get_market_caps(tickers)
+    print(market_caps)
 
     # it is cleaner code to break up operations into
     # well-named, descriptive/contextual variables. but we could do:
     # print(get_stock_bids('TSLA', 'XOM', 'ORCL']))
 
-    print(fetch_bid('AAPL'))
 
 if __name__ == '__main__':
     sys.exit(main())
--- scraper/sfa_scraper.py
+++ scraper/sfa_scraper.py
@@ -14,12 +14,49 @@ def get_stock_bids(companies):
 
 
 def fetch_bid(ticker):
-        summary_url = 'https://finance.yahoo.com/quote/' + ticker + '?p=' + ticker
-        response = urlopen(summary_url)
+    summary_url = 'https://finance.yahoo.com/quote/' + ticker + '?p=' + ticker
+    response = urlopen(summary_url)
 
-        html = BeautifulSoup(response, "html.parser")
+    html = BeautifulSoup(response, "html.parser")
 
-        qt = html.find(id = "quote-summary")
-        bid = qt.find(attrs={"data-test":"BID-value"}).getText()
+    quote = html.find(id = "quote-summary")
+    bid = quote.find(attrs={"data-test":"BID-value"}).getText()
 
-        return [float(item) for item in bid.split('x')]
+    return [float(item) for item in bid.split('x')]
+
+
+def get_market_caps(companies):
+    stock_mkt_caps = {}
+
+    for company in companies:
+        mkt_cap = fetch_market_cap(company)
+        print("The market cap of {} is {} ".format(company, mkt_cap))
+
+        stock_mkt_caps[company] = {"Market Cap" : mkt_cap}
+
+    return stock_mkt_caps
+
+
+def fetch_market_cap(ticker):
+    summary_url = 'https://finance.yahoo.com/quote/' + ticker + '?p=' + ticker
+    response = urlopen(summary_url)
+
+    html = BeautifulSoup(response, "html.parser")
+
+    quote = html.find(id = "quote-summary")
+    market_cap = quote.find(attrs={"data-test":"MARKET_CAP-value"}).getText()
+
+    mkt_cap = parse_market_cap(market_cap)
+    return mkt_cap
+
+def parse_market_cap(string):
+    multipliers = {'K':1000, 'M':1000000, 'B':1000000000}
+
+    if string[-1].isdigit(): # check if no suffix
+        return int(string)
+
+    letter = string[-1]
+    string = string[:-1]
+    mult = multipliers[letter]
+
+    return float(string) * int(mult)
